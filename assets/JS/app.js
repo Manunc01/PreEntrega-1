ba70869b6210b80582961e46963bcda7f42722b5
Manuel Noceti Campos <mnoceti86@gmail.com>
2022-11-25 20:52:13 -0300

    cambios en el for loop

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -3,6 +3,11 @@ let dinero = parseInt (prompt("ingresa tu dinero"))
 
 let totalApagar= 500
 
+alert("procesando compra...")
+for (let i = 0; i <= 3; i++) {
+    alert("paso: "+i+"/3");
+}
+
 
 if (dinero >= totalApagar) {
     alert("compra realizada")
@@ -10,7 +15,7 @@ if (dinero >= totalApagar) {
     alert("Te quedan $"+dinero)
 } 
 else {
-    let tarjeta = parseInt (prompt("Si tienes tarjeta de credito pon 1, si no, pon 2"))
+    let tarjeta = parseInt (prompt("dinero insuficiente, si tienes tarjeta de credito pon 1, si no, pon 2"))
     if (tarjeta === 1){
         alert("Compraste con la tarjeta de credito")
     }
