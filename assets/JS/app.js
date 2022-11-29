ab89556500768db18d463a6261b559eee078a1f5
Manuel Noceti Campos <mnoceti86@gmail.com>
2022-11-28 23:40:43 -0300

    agregando mas css y js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,18 +1,18 @@
+function resta (num1,num2){
+    num1=num1-num2
+    console.log ("te quedan $"+num1)}
 
 let dinero = parseInt (prompt("ingresa tu dinero"))
 
 let totalApagar= 500
 
-alert("procesando compra...")
-for (let i = 0; i <= 3; i++) {
-    alert("paso: "+i+"/3");
-}
-
-
+	alert("procesando compra...")
+	for (let i = 0; i <= 3; i++) {
+	    alert("paso: "+i+"/3");
+	}
 if (dinero >= totalApagar) {
     alert("compra realizada")
-    dinero = dinero - totalApagar
-    alert("Te quedan $"+dinero)
+    resta(dinero,totalApagar)
 } 
 else {
     let tarjeta = parseInt (prompt("dinero insuficiente, si tienes tarjeta de credito pon 1, si no, pon 2"))
